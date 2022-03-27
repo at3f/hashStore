bfb2e4a130e8e6acd97d15365bbf5dcd05c10663
at3f <ahmed0103060120@gmail.com>
2022-03-27 04:19:28 +0200

    27/3

--- routes/user.js
+++ routes/user.js
@@ -10,12 +10,10 @@ router.post('/user/logout',cUser.postLogout)
 router.post('/user/getNewAccessToken',cUser.getNewAccessToken)
 router.put('/user/updatePassword',jtoken.authorize,cUser.UpdatePassword)
 router.delete('/user/deleteAccount',jtoken.authorize,cUser.deleteUser,cUser.postLogout)
-
 router.get('/user/getUserData',jtoken.authorize,cUser.getUserData)
 router.post('/user/forgetPassword',cUser.sendOTP)
 router.post('/user/sendCode',cUser.sendCode)
 router.post('/user/resetPassword',jtoken.verifyToken,cUser.resetPassword)
-
 router.post('/user/sendVerificationMail',jtoken.authorize,cUser.sendVerificationMail)
 router.get('/user/VerificationMail/:id/:code',cUser.VerificationMail)
 
